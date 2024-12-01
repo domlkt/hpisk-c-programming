5c1b8d9c507f059102db985a9077b39344bc62d4
domlkt <domlkt45@gmail.com>
2024-12-01 12:23:53 +0100

    first commit

---
+++ helloworld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*Das ist ein Kommentar. Kommentare werden bei der Ausführung des
+Programms nicht berücksichtigt. Kommentare sind nützlich, um sich Notizen
+im Quellcode zu machen oder Programmteile bei Ausführung unberücksichtigt
+zu lassen.*/
+
+int main()
+{
+    printf("Hello, world!");
+    return 0;
+}
